79f6ec35e619e0dd90209abfa1df96a5368e437a
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-08 13:00:57 -0300

    Exercicio sobre valor 0

---
+++ Cap3-nivel1/ExercicioCap3Nivel1ATV2.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
