ca3016a1cc80fa425de77a5463a1e9c59f807f05
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-08 13:09:52 -0300

    Atividade 4

---
+++ Cap3-nivel1/ExercicioCap3Nivel1ATV4.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type coibroxcoin int
+
+var x coibroxcoin
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+
+}
